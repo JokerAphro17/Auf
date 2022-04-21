57c26f772db78d4cd60c164d73e7550381c57c31
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-04-21 15:39:23 +0000

    fisrt

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/bootstrap5/css/bootstrap.min.css">
+    <script src="../assets/bootstrap5/js/bootstrap.bundle.js" defer></script>
+    <title>traitement</title>
+</head>
+<body>
+    
+</body>
+</html>
